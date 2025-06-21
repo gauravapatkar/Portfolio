0dfe3cf03b49faa9e5becbc69f414401e25978ba
Gaurav Anand Patkar <138325334+gauravapatkar@users.noreply.github.com>
2025-06-21 23:06:29 +0530

    Update ProjectCradData.js

--- src/components/projects/ProjectCradData.js
+++ src/components/projects/ProjectCradData.js
@@ -1,10 +1,77 @@
+// import ToDoList from "../../assets/videos/ToDoList.mp4";
+// import Calculator from "../../assets/videos/CalculatorJs.mp4";
+// import Netflix from "../../assets/videos/netflixCV.mp4";
+// import SignIn from "../../assets/videos/SignIn.mp4";
+// import Airbnb from "../../assets/videos/Airbnb.mp4";
+
+// const ProjectCradData = [
+//   {
+//     videosrc: ToDoList,
+//     title: "Project Title :-",
+//     titleName: "To-Do-List",
+//     Text: "This is my To-Do List Project. It is a simple and interactive web application built by me using HTML, CSS, and JavaScript. It allows users to add, delete, and mark tasks as complete. This project demonstrates proficiency in DOM manipulation, responsive design, and basic CRUD operations on tasks. This Project idea is for showcasing  my skills in front-end development and user interface design.",
+//     view: "https://gauravapatkar.github.io/ToDoList/",
+//   },
+//   {
+//     videosrc: Calculator,
+//     title: "Project Title :- ",
+//     titleName: "Calculator",
+//     Text: " This is my Calculator Project. I Developed a fully functional calculator using HTML, CSS, and JavaScript. This project includes a user-friendly interface for basic arithmetic operations such as addition, subtraction, multiplication, and division, featuring responsive design and efficient event handling for seamless user interaction.",
+//     view: "https://gauravapatkar.github.io/Calculator-JS/",
+//   },
+//   {
+//     videosrc: SignIn,
+//     title: "Project Title :- ",
+//     titleName: "SignIn",
+
+//     Text: " This is my SignIn Project. A simple web-based project designed using HTML, CSS, and JavaScript. This project features a user-friendly SignIn form with interactive design and responsive layout. It captures user input for fields such as username and password but does not include authentication or backend integration. Ideal for showcasing front-end development skills like form validation, styling, and basic interactivity.",
+//     view: "https://gauravapatkar.github.io/SignIn-Register/#",
+//   },
+//   {
+//     videosrc: Netflix,
+//     title: "Project Title :- ",
+//     titleName: "Netflix",
+//     Text: "This is my Netflix homepage clone project that i am developed. This project is responsive using HTML and CSS, incorporating modern web design practices and techniques to replicate the platform's layout and user interface for an engaging visual experience.",
+//     view: "https://gauravapatkar.github.io/netflix/",
+//   },
+
+//   {
+//     videosrc: Airbnb,
+//     title: "Project Title :-",
+//     titleName: "Airbnb",
+
+//     Text: "This is my Airbnb Homepage Clone project that i developed using HTML and CSS with responsivenes. The project replicates the layout, design, and visual elements of the original Airbnb website, focusing on clean, structured code and cross-browser compatibility.",
+//     view: "https://gauravapatkar.github.io/Airbnb-clone/",
+//   },
+// ];
+
+// export default ProjectCradData;
+
+
+
 import ToDoList from "../../assets/videos/ToDoList.mp4";
 import Calculator from "../../assets/videos/CalculatorJs.mp4";
 import Netflix from "../../assets/videos/netflixCV.mp4";
 import SignIn from "../../assets/videos/SignIn.mp4";
 import Airbnb from "../../assets/videos/Airbnb.mp4";
+import QuizApp from "../../assets/videos/quize_appR.mp4";
+import DoctorBookingApp from "../../assets/videos/doctorBooR.mp4";
 
 const ProjectCradData = [
+  {
+    videosrc: DoctorBookingApp,
+    title: "Project Title :-",
+    titleName: "Doctor-Booking-App",
+    Text: "Developed a responsive online appointment booking platform using React.js, enabling users to schedule appointments with doctors seamlessly.Implemented doctor listing with dynamic filtering by specialty, allowing users to easily find and select relevant medical professionals.Integrated a date and time slot picker within a 7-day availability window using React state management and controlled components",
+    view: "https://gauravapatkar.github.io/Doctor-Booking-App/",
+  },
+  {
+    videosrc: QuizApp,
+    title: "Project Title :-",
+    titleName: "QuizApp",
+    Text: "Developed a responsive Quiz Application using HTML,CSS, and vanilla JavaScript, providing a smooth user experience across devices. Implemented dynamic question rendering and scoring logic using JavaScript, enabling real-time feedback and result calculation.Displayed detailed quiz results after submission, showing the total score,correct answers, incorrect answers.",
+    view: "https://gauravapatkar.github.io/quiz_app/",
+  },
   {
     videosrc: ToDoList,
     title: "Project Title :-",
