1225873b03cca7742820be64b0fda1e768de0bfd
Gaurav Anand Patkar <138325334+gauravapatkar@users.noreply.github.com>
2025-06-23 23:22:13 +0530

    Update navbar.js - added scrooling effect

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,72 +1,55 @@
 import React, { useState } from "react";
 import "./navbar.css";
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
 import menu from "../../assets/menu.png";
+
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="Menu">
         <Link
           activeClass="active"
-          to="/"
-          // spy={true}
-          // smooth={true}
-          // offset={-100}
-          // duration={500}
+          to="home"
+          smooth={true}
+          duration={500}
+          offset={-70}
           className="MenuList"
         >
           Home
         </Link>
         <Link
           activeClass="active"
-          to="/About"
-          // spy={true}
-          // smooth={true}
-          // offset={-80}
-          // duration={500}
+          to="about"
+          smooth={true}
+          duration={500}
+          offset={-70}
           className="MenuList"
         >
           About
         </Link>
         <Link
           activeClass="active"
-          to="/Projects"
-          // spy={true}
-          // smooth={true}
-          // offset={-80}
-          // duration={500}
+          to="projects"
+          smooth={true}
+          duration={500}
+          offset={-70}
           className="MenuList"
         >
-          Project
+          Projects
         </Link>
         <Link
           activeClass="active"
-          to="/contact"
-          // spy={true}
-          // smooth={true}
-          // offset={-80}
-          // duration={500}
+          to="contact"
+          smooth={true}
+          duration={500}
+          offset={-70}
           className="MenuList"
         >
           Contact
         </Link>
-        {/* <ul className="MenuList">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/About">About</Link>
-          </li>
-          <li>
-            <Link to="/Projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul> */}
       </div>
-
       <img
         src={menu}
         alt="Menu"
@@ -77,36 +60,15 @@ const Navbar = () => {
       />
 
       <div className="navMenu" style={{ display: showMenu ? "flex" : "none" }}>
-        {/* <ul className="MenuList">
-          <li className="navMenuList">
-            <Link to="/" onClick={() => setShowMenu(false)}>
-              Home
-            </Link>
-          </li>
-          <li className="navMenuList">
-            <Link to="/About" onClick={() => setShowMenu(false)}>
-              About
-            </Link>
-          </li>
-          <li className="navMenuList">
-            <Link to="/Projects" onClick={() => setShowMenu(false)}>
-              Projects
-            </Link>
-          </li>
-          <li className="navMenuList">
-            <Link to="/contact" onClick={() => setShowMenu(false)}>
-              Contact
-            </Link>
-          </li>
-        </ul> */}
+     
 
         <Link
           activeClass="active"
-          to="/"
-          // spy={true}
-          // smooth={true}
-          // offset={-100}
-          // duration={500}
+          to="home"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
           className="navMenuList"
           onClick={() => setShowMenu(false)}
         >
@@ -114,11 +76,11 @@ const Navbar = () => {
         </Link>
         <Link
           activeClass="active"
-          to="/About"
-          // spy={true}
-          // smooth={true}
-          // offset={-50}
-          // duration={500}
+          to="about"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
           className="navMenuList"
           onClick={() => setShowMenu(false)}
         >
@@ -126,11 +88,11 @@ const Navbar = () => {
         </Link>
         <Link
           activeClass="active"
-          to="/Projects"
-          // spy={true}
-          // smooth={true}
-          // duration={500}
-          // offset={-70}
+          to="projects"
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-70}
           className="navMenuList"
           onClick={() => setShowMenu(false)}
         >
@@ -138,11 +100,7 @@ const Navbar = () => {
         </Link>
         <Link
           activeClass="active"
-          to="/contact"
-          // spy={true}
-          // smooth={true}
-          // offset={-100}
-          // duration={500}
+          to="contact"
           className="navMenuList"
           onClick={() => setShowMenu(false)}
         >
@@ -154,3 +112,199 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import "./navbar.css";
+// import { Link } from "react-router-dom";
+// import menu from "../../assets/menu.png";
+// const Navbar = () => {
+//   const [showMenu, setShowMenu] = useState(false);
+//   return (
+//     <nav className="navbar">
+//       <div className="Menu">
+//         <Link
+//           activeClass="active"
+//           to="/"
+//           // spy={true}
+//           // smooth={true}
+//           // offset={-100}
+//           // duration={500}
+//           className="MenuList"
+//         >
+//           Home
+//         </Link>
+//         <Link
+//           activeClass="active"
+//           to="/About"
+//           // spy={true}
+//           // smooth={true}
+//           // offset={-80}
+//           // duration={500}
+//           className="MenuList"
+//         >
+//           About
+//         </Link>
+//         <Link
+//           activeClass="active"
+//           to="/Projects"
+//           // spy={true}
+//           // smooth={true}
+//           // offset={-80}
+//           // duration={500}
+//           className="MenuList"
+//         >
+//           Project
+//         </Link>
+//         <Link
+//           activeClass="active"
+//           to="/contact"
+//           // spy={true}
+//           // smooth={true}
+//           // offset={-80}
+//           // duration={500}
+//           className="MenuList"
+//         >
+//           Contact
+//         </Link>
+//         {/* <ul className="MenuList">
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/About">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/Projects">Projects</Link>
+//           </li>
+//           <li>
+//             <Link to="/contact">Contact</Link>
+//           </li>
+//         </ul> */}
+//       </div>
+
+//       <img
+//         src={menu}
+//         alt="Menu"
+//         className="mobMenu"
+//         onClick={() => {
+//           setShowMenu(!showMenu);
+//         }}
+//       />
+
+//       <div className="navMenu" style={{ display: showMenu ? "flex" : "none" }}>
+//         {/* <ul className="MenuList">
+//           <li className="navMenuList">
+//             <Link to="/" onClick={() => setShowMenu(false)}>
+//               Home
+//             </Link>
+//           </li>
+//           <li className="navMenuList">
+//             <Link to="/About" onClick={() => setShowMenu(false)}>
+//               About
+//             </Link>
+//           </li>
+//           <li className="navMenuList">
+//             <Link to="/Projects" onClick={() => setShowMenu(false)}>
+//               Projects
+//             </Link>
+//           </li>
+//           <li className="navMenuList">
+//             <Link to="/contact" onClick={() => setShowMenu(false)}>
+//               Contact
+//             </Link>
+//           </li>
+//         </ul> */}
+
+//         <Link
+//           activeClass="active"
+//           to="/"
+//           // spy={true}
+//           // smooth={true}
+//           // offset={-100}
+//           // duration={500}
+//           className="navMenuList"
+//           onClick={() => setShowMenu(false)}
+//         >
+//           Home
+//         </Link>
+//         <Link
+//           activeClass="active"
+//           to="/About"
+//           // spy={true}
+//           // smooth={true}
+//           // offset={-50}
+//           // duration={500}
+//           className="navMenuList"
+//           onClick={() => setShowMenu(false)}
+//         >
+//           About
+//         </Link>
+//         <Link
+//           activeClass="active"
+//           to="/Projects"
+//           // spy={true}
+//           // smooth={true}
+//           // duration={500}
+//           // offset={-70}
+//           className="navMenuList"
+//           onClick={() => setShowMenu(false)}
+//         >
+//           Project
+//         </Link>
+//         <Link
+//           activeClass="active"
+//           to="/contact"
+//           // spy={true}
+//           // smooth={true}
+//           // offset={-100}
+//           // duration={500}
+//           className="navMenuList"
+//           onClick={() => setShowMenu(false)}
+//         >
+//           Contact
+//         </Link>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
