2c210b60833f785a1ac7f77a6de42365ea20320a
Gaurav Anand Patkar <138325334+gauravapatkar@users.noreply.github.com>
2025-06-23 23:21:15 +0530

    Update App.js - added scrolling to app.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import Home from "./routes/Home.jsx";
 import About from "./routes/About.jsx";
 import Projects from "./routes/Pprojects.jsx";
 import Contacts from "./routes/Contacts.jsx";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Element } from "react-scroll";
+
+// import { Routes, Route, BrowserRouter } from "react-router-dom";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Projects" element={<Projects />} />
-        <Route path="/contact" element={<Contacts />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/About" element={<About />} />
+    //     <Route path="/Projects" element={<Projects />} />
+    //     <Route path="/contact" element={<Contacts />} />
+    //   </Routes>
+    // </BrowserRouter>
+
+<div>
+      <Navbar />
+      <Element name="home">
+        <Home />
+      </Element>
+      <Element name="about">
+        <About />
+      </Element>
+      <Element name="projects">
+        <Pprojects />
+      </Element>
+      <Element name="contact">
+        <Contacts />
+      </Element>
+    </div>
   );
 }
 
